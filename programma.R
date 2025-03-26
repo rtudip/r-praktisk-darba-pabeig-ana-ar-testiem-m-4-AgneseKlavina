bb2257bb80248503fd139acecd5fe65951706e32
AgneseKlavina <144719522+AgneseKlavina@users.noreply.github.com>
2025-03-26 17:00:43 +0000

    2.mēģ.- pievienoju stringAsFactors=TRUE, izmantoju sapply un tapply, lai apstradatas vertibas butu skaitliskas.

--- programma.R
+++ programma.R
@@ -1,43 +1,35 @@
-kordat <- read.table("variants15.txt", header=TRUE, sep="\t", dec=",", strip.white=TRUE, row.names=1)
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(magrittr)
+library(svglite)
 
+kordat <- read.table("variants15.txt", dec=",", strip.white=TRUE, stringsAsFactors=TRUE, row.names=1, header=TRUE, sep="\t")
 kordat[, 9:ncol(kordat)] <-lapply(kordat[, 9:ncol(kordat)], as.factor)
 
 sink("results.txt")
-kopsavilkums <- lapply(kordat[, 9:ncol(kordat)], table)
-print(kopsavilkums)
+
+summary(kordat[, 9:ncol(kordat)])
 
 sl.by.b <-split(kordat$Slope, kordat$b)
 print(sl.by.b)
 
-
-kordat$Slope <-as.numeric(as.character(kordat$Slope))
-kordat$Intercept <-as.numeric(as.character(kordat$Intercept))
-kordat$adj.r.squared <-as.numeric(as.character(kordat$adj.r.squared))
 kordat$Average <- rowMeans(kordat[, c("Slope", "Intercept", "adj.r.squared")], na.rm=TRUE)
 
-library(dplyr)
-standartnovirze<-kordat %>%
-    group_by(f) %>%
-    summarise(standartnovirze=sd(Intercept, na.rm=TRUE))
+
+standartnovirze<- sapply(kordat[, 9:ncol(kordat)], function(x) tapply(as.numeric(x), kordat$f, sd, na.rm=TRUE))
 print(standartnovirze)
 
-prockordat <- kordat %>%
-    filter(ifelse(adj.r.squared>0, adj.r.squared>0.7, adj.r.squared>-0.3))
+prockordat <- kordat %>% filter(adj.r.squared>ifelse(all(kordat$adj.r.squared>0), 0.7, -0.3))
 
-prockordat$Slope <- 1 - (1 / prockordat$Slope)
+prockordat$Slope <- sapply(prockordat$Slope, function(y) 1-1/y)
 print(prockordat)
-
-library(ggplot2)
-scatter <- ggplot(kordat, aes(x = MAD, y = Average)) +
-  geom_point() +
-  labs(title = "Scatter plot", x = "MAD", y = "Average")
-ggsave("scatter.svg", plot=scatter, width=8, height=8)
-
-kordat <- read.table("variants15.txt", header = TRUE, sep = "\t", dec = ",", strip.white = TRUE, row.names = 1)
-kordat$f <- as.factor(kordat$f)
-box <- ggplot(kordat, aes(x = f, y = Intercept, fill = f)) +
-  geom_boxplot() +
-  labs(title = "Boxplot pec f faktora")
-ggsave("boxplot.svg", plot=box, width=8, height=8)
-
 sink()
+
+svg("scatter.svg")
+plot(kordat$MAD, kordat$Average, xlab="MAD", ylab="Average", main="Scatter plot")
+dev.off()
+
+svg("boxplot.svg")
+boxplot(Intercept~f, data=kordat, col=rainbow(length(unique(kordat$f))), main="boxplot no f faktra")
+dev.off()
